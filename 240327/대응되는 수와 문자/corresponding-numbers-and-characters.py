32b9732105a0be3a1326b1ee5939816ebb34036d
서민주 <sminju1009@gmail.com>
2024-03-27 22:17:28 +0900

    Title: 대응되는 수와 문자, Time: 592ms, Memory: 43MB, Status: Passed - CodeTree

--- 240327/대응되는 수와 문자/corresponding-numbers-and-characters.py
+++ 240327/대응되는 수와 문자/corresponding-numbers-and-characters.py
@@ -1,18 +1,20 @@
 n, m = map(int, input().split())
 
-dic = dict()
+# 딕셔너리를 사용하여 숫자와 문자열 간의 대응 관계를 저장
+number_to_string = {}
+string_to_number = {}
 
-for i in range(n):
-    string = input()
-    dic[string] = str(i + 1)
+for i in range(1, n + 1):
+    s = input().strip()
+    number_to_string[i] = s
+    string_to_number[s] = i
 
 for _ in range(m):
-    query = input()
+    query = input().strip()
     if query.isdigit():  # 만약 쿼리가 숫자인 경우
         number = int(query)
-        for key, value in dic.items():
-            if value == query:
-                print(key)
-                break
+        if number in number_to_string:  # 딕셔너리에 해당 숫자가 존재하는지 확인
+            print(number_to_string[number])  # 대응되는 문자열 출력
     else:  # 만약 쿼리가 문자열인 경우
-        print(dic.get(query, 'None'))
+        if query in string_to_number:  # 딕셔너리에 해당 문자열이 존재하는지 확인
+            print(string_to_number[query])  # 대응되는 숫자 출력
