2d3ce754443032c8597639ec2c286c229ca66e78
서민주 <sminju1009@gmail.com>
2024-03-28 21:36:02 +0900

    Title: 두 수의 합, Time: 110ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240328/두 수의 합/sum-of-two-num.py
+++ 240328/두 수의 합/sum-of-two-num.py
@@ -1,10 +1,11 @@
-n, m = map(int, input().split())
+n, k = map(int, input().split())
 arr = list(map(int, input().split()))
 
 cnt = 0
-for i in range(n-1):
-    for j in range(i+1, n):
-        if i!=j and arr[i]+arr[j]==m:
-                cnt += 1
 
-print(cnt)
+for i in range(n):
+    res = k - arr[i]
+    if res in arr:
+        cnt += 1
+
+print(cnt//2)
