690f5b9a43719e71f57a1110093d2799e24d4254
서민주 <sminju1009@gmail.com>
2024-07-18 11:11:28 +0900

    Title: 바이러스 백신, Time: 292ms, Memory: 28MB, Status: Passed - CodeTree

--- 240718/바이러스 백신/vaccine-for-virus.py
+++ 240718/바이러스 백신/vaccine-for-virus.py
@@ -1,48 +1,41 @@
+from itertools import combinations
 from collections import deque
 
-def dfs(level):
-    if level==m:
-        return
-    for y, x in dots:
-        index = dots.index((y, x))
-        if v_dots[index] == 0:
-            bfs(y, x)
-            v_dots[index] = 1
-            dfs(level+1)
-            v_dots[index] = 0
-
-def bfs(y, x):
+def bfs(hospitals):
+    visited = [[-1] * n for _ in range(n)]
     q = deque()
-    q.append((y, x, 1))
-    visited[y][x] = 1
-
+    for y, x in hospitals:
+        q.append((y, x))
+        visited[y][x] = 0
+        
+    max_time = 0
+    
     while q:
-        y, x, level = q.popleft()
+        y, x = q.popleft()
         for dy, dx in ((0, -1), (-1, 0), (0, 1), (1, 0)):
-            ny, nx = dy+y, dx+x
-            if 0<=ny<n and 0<=nx<n and arr[ny][nx]!=1 and (visited[ny][nx]==0 or visited[ny][nx]>level):
-                q.append((ny, nx, level+1))
-                visited[ny][nx] = level+1
+            ny, nx = dy + y, dx + x
+            if 0 <= ny < n and 0 <= nx < n and arr[ny][nx] != 1 and visited[ny][nx] == -1:
+                visited[ny][nx] = visited[y][x] + 1
+                q.append((ny, nx))
+                if arr[ny][nx] == 0:
+                    max_time = max(max_time, visited[ny][nx])
+                    
+    # Check if there's any unvisited area that is not a wall
+    for i in range(n):
+        for j in range(n):
+            if arr[i][j] == 0 and visited[i][j] == -1:
+                return float('inf')  # If any area is still unvisited, return infinity
+
+    return max_time
 
 n, m = map(int, input().split())
 arr = [list(map(int, input().split())) for _ in range(n)]
-visited = [[0]*n for _ in range(n)] # 방문 표시 배열
-dots = [] # 병원을 받을 배열
-ans = 0 # 바이러스 없애는데 걸리는 시간의 최솟값 구하기
 
-for i in range(n):
-    for j in range(n):
-        if arr[i][j]==2:
-            dots.append((i, j))
-v_dots = [0] * len(dots) # dfs 함수에서 이용할 방문 배열 체크
+dots = [(i, j) for i in range(n) for j in range(n) if arr[i][j] == 2]
+min_time = float('inf')
 
-dfs(0)
+for hospitals in combinations(dots, m):
+    time = bfs(hospitals)
+    min_time = min(min_time, time)
 
-for i in range(n):
-    for j in range(n):
-        if (arr[i][j]==0 or arr[i][j]==2) and visited[i][j]==0:
-            ans = -1
-            break
-        ans = max(visited[i][j], ans)
-
-print(ans)
+print(min_time if min_time != float('inf') else -1)
