1c0b69ebaf1f23e48efd9a0eedf863c42bc3b83a
서민주 <sminju1009@gmail.com>
2024-07-18 10:07:59 +0900

    Title: 바이러스 백신, Time: 162ms, Memory: 25MB, Status: Wrong Answer - CodeTree

--- 240718/바이러스 백신/vaccine-for-virus.py
+++ 240718/바이러스 백신/vaccine-for-virus.py
@@ -25,20 +25,7 @@ def bfs(y, x):
                 visited[ny][nx] = level+1
 
 n, m = map(int, input().split())
-arr = [list(map(int, input().split())) for _ in ran
-        for dy, dx in ((0, -1), (-1, 0), (0, 1), (1, 0)):
-            ny, nx = dy+y, dx+x
-            if 0<=ny<n and 0<=nx<n and arr[ny][nx]!=1 and (visited[ny][nx]==0 or visited[ny][nx]>level):
-                q.append((ny, nx, level+1))
-                visited[ny][nx] = level+1
-
-n, m = map(int, input().split())
 arr = [list(map(int, input().split())) for _ in range(n)]
-visited = [[0]*n for _ in range(n)] # 방문 표시 배열
-dots = [] # 병원을 받을 배열
-
-14def bfs(y, x):
-$0ge(n)]
 visited = [[0]*n for _ in range(n)] # 방문 표시 배열
 dots = [] # 병원을 받을 배열
 ans = 0 # 바이러스 없애는데 걸리는 시간의 최솟값 구하기
